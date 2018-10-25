980ca03f1f8a14b22b6e30c67a47e99e6d9eb436
MJNeff18579 <MJNeff18579@CC121030.students.school.marmion.org>
2018-10-25 13:31:50 -0500

    Initial Commit

---
+++ src/pollproject/PollTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pollproject;
+
+/**
+ *
+ * @author MJNeff18579
+ */
+public class PollTest {
+    public static void main(String[] args) {
+        PollDisplayPanel votingMachine = new PollDisplayPanel("Tami", "Brian", "Liz");
+        votingMachine.vote1();
+        votingMachine.vote2();
+        votingMachine.vote2();
+        System.out.println(votingMachine);
+    }
+}
